072928b312495894a04752b7d1f910fbc29a3256
Jung's Study <JungsArk@gmail.com>
2026-07-22 10:50:04 +0900

    feat : 토스 관련 DTO 및 Config, 예외, PG 설정

---
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/infrastructure/toss/dto/TossErrorResponse.java
@@ -0,0 +1,10 @@
+package com.growmighty.lectures.firstday.payment.infrastructure.toss.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record TossErrorResponse(
+    String code,
+    String message
+) {
+}
