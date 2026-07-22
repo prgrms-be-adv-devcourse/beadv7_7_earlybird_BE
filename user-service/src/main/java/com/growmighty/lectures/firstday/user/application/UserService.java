20161fa7981d1a2ce169901052b30eb261c2b0f6
Kim Hanahan <krkhnh@gmail.com>
2026-07-22 16:12:11 +0900

    feat: BCryptPasswordEncoder 도입 및 PasswordEncoder 리팩토링
    - Sha256PasswordEncoder 제거 및 BCryptPasswordEncoder로 교체
    - SecurityConfig에 PasswordEncoder 빈 등록
    - 비밀번호 인코딩이 salt 방식으로 동작하도록 개선
    - SecurityConfigTest 추가하여 PasswordEncoder 동작 검증

--- user-service/src/main/java/com/growmighty/lectures/firstday/user/application/UserService.java
+++ user-service/src/main/java/com/growmighty/lectures/firstday/user/application/UserService.java
@@ -10,6 +10,7 @@ import com.growmighty.lectures.firstday.user.domain.CreatorProfileRepository;
 import com.growmighty.lectures.firstday.user.domain.User;
 import com.growmighty.lectures.firstday.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
