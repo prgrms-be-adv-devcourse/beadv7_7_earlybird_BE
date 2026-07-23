0a411d7ef1c522fc22ffc4f28345e57ac3e63faf
Jung's Study <JungsArk@gmail.com>
2026-07-23 16:25:00 +0900

    feat : 타임아웃 결제 복구 스케쥴러 추가

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/PaymentServiceApplication.java
@@ -3,8 +3,10 @@ package com.growmighty.lectures.firstday.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
     "com.growmighty.lectures.firstday.payment",
     "com.growmighty.lectures.firstday.common"   // 이게 없으면 GlobalExceptionHandler가 빈으로 안 뜬다
---
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/application/PaymentRecoveryScheduler.java
@@ -0,0 +1,25 @@
+package com.growmighty.lectures.firstday.payment.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentRecoveryScheduler {
+
+    private final PaymentRecoveryBatchService paymentRecoveryBatchService;
+
+
+    /**
+     * Config 서버에 필요하면 복구 주기를 변경할 수 있다.
+     * 예시 :
+     payment:
+     recovery:
+     schedule-fixed-delay: 180000
+     */
+    @Scheduled(fixedDelayString = "${payment.recovery.schedule-fixed-delay:18000}")
+    public void recoveryTimedOutPayments() {
+        paymentRecoveryBatchService.recoverTimedOutPayments();
+    }
+}
