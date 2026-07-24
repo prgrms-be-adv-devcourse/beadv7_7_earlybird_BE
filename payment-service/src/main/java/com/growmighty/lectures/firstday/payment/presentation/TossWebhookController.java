2d6ca9249a8c0cdb916666e48a8fe74022709b08
Jung's Study <JungsArk@gmail.com>
2026-07-24 11:06:51 +0900

    feat : Toss 웹훅 컨트롤러 추가

---
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/presentation/TossWebhookController.java
@@ -0,0 +1,26 @@
+package com.growmighty.lectures.firstday.payment.presentation;
+
+import com.growmighty.lectures.firstday.payment.application.PaymentConfirmationService;
+import com.growmighty.lectures.firstday.payment.application.PaymentGateway;
+import com.growmighty.lectures.firstday.payment.infrastructure.toss.dto.TossWebhookRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/v1/payments/toss")
+public class TossWebhookController {
+    private final PaymentGateway paymentGateway;
+    private final PaymentConfirmationService paymentConfirmationService;
+
+    @PostMapping("/webhook")
+    public void receive(@RequestBody TossWebhookRequest request) {
+        String paymentKey = request.data().paymentKey();
+
+        PaymentGateway.PgPayment pgPayment = paymentGateway.getPayment(paymentKey);
+        paymentConfirmationService.reconcile(pgPayment);
+    }
+}
