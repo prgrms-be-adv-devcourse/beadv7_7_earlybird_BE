e2bf37a8fb19956fd85176a8c4d81ba3e66d1632
Jung's Study <JungsArk@gmail.com>
2026-07-24 09:15:32 +0900

    fix : Spring 의 validation 어노테이션 적용으로 자동 검증 적용

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/config/PaymentRecoveryProperties.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/config/PaymentRecoveryProperties.java
@@ -1,36 +1,23 @@
 package com.growmighty.lectures.firstday.payment.config;
 
+import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.time.DurationMin;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
 @ConfigurationProperties(prefix = "payment.recovery")
+@Validated
 public record PaymentRecoveryProperties(
+
+    @DefaultValue("PT3M")
+    @DurationMin(seconds = 1)
     Duration confirmationTimeOut,
+
+    @DefaultValue("100")
+    @Positive
     Integer batchSize
 ) {
-
-    /**
-     * TimeOut 시간은 3분으로 결정했습니다.
-     * @param confirmationTimeOut
-     * @param batchSize
-     */
-    public PaymentRecoveryProperties {
-        confirmationTimeOut = confirmationTimeOut == null
-            ? Duration.ofMinutes(3)
-            : confirmationTimeOut;
-
-        batchSize = batchSize == null
-            ? 100
-            : batchSize;
-
-
-        if (confirmationTimeOut.isZero() || confirmationTimeOut.isNegative()) {
-            throw new IllegalArgumentException("결제 승인 타임아웃은 0보다 커야 합니다.");
-        }
-
-        if (batchSize <= 0) {
-            throw new IllegalArgumentException("결제 복구 배치 크기는 0보다 커야합니다.");
-        }
-    }
 }
