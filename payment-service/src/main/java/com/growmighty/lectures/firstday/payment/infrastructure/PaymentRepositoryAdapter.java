294675a954095c9e1c36dd4be9a61046e1bdb350
Jung's Study <JungsArk@gmail.com>
2026-07-27 14:28:25 +0900

    fix : PaymentRepositoryAdapter의 findByOrderId에 필요한 타입을 UUID로 변경

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/infrastructure/PaymentRepositoryAdapter.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/infrastructure/PaymentRepositoryAdapter.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -32,7 +33,7 @@ public class PaymentRepositoryAdapter implements PaymentRepository {
     }
 
     @Override
-    public Optional<Payment> findByOrderId(Long orderId) {
+    public Optional<Payment> findByOrderId(UUID orderId) {
         return jpaRepository.findByOrderId(orderId);
     }
 
