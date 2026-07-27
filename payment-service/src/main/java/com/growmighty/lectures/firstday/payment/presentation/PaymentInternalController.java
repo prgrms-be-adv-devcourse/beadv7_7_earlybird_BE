d2f2655065d229d7a7e19115e5b46c0c0fcd6930
Jung's Study <JungsArk@gmail.com>
2026-07-27 17:23:54 +0900

    feat : 내부 환불 API 추가

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/presentation/PaymentInternalController.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/presentation/PaymentInternalController.java
@@ -5,12 +5,15 @@ import com.growmighty.lectures.firstday.payment.application.PaymentService;
 import com.growmighty.lectures.firstday.payment.application.dto.PaymentPreparationInfo;
 import com.growmighty.lectures.firstday.payment.presentation.dto.PaymentPrepareRequest;
 import com.growmighty.lectures.firstday.payment.presentation.dto.PaymentPrepareResponse;
+import com.growmighty.lectures.firstday.refund.Presentation.dto.PaymentRefundRequest;
+import com.growmighty.lectures.firstday.refund.Presentation.dto.PaymentRefundResponse;
+import com.growmighty.lectures.firstday.refund.application.RefundService;
+import com.growmighty.lectures.firstday.refund.domain.Refund;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentInternalController {
 
     private final PaymentService paymentService;
+    private final RefundService refundService;
 
     @PostMapping("/prepare")
     public PaymentPrepareResponse prepare(@Valid @RequestBody PaymentPrepareRequest request) {
@@ -26,5 +30,11 @@ public class PaymentInternalController {
             request.amount()
         );
         return PaymentPrepareResponse.from(payment);
+    }
+
+    @PostMapping("/orders/{orderId}/refund")
+    public PaymentRefundResponse refundResponse(@PathVariable UUID orderId, @Valid @RequestBody PaymentRefundRequest request) {
+        Refund refund = refundService.refund(orderId, request.reason());
+        return  PaymentRefundResponse.from(refund);
     }
 }
