28f702713e7bc8613a596b1bf9258b5742c495a2
Jung's Study <JungsArk@gmail.com>
2026-07-30 16:06:02 +0900

    fix : 누락된 refund 스캔범위 추가

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/PaymentServiceApplication.java
@@ -4,12 +4,22 @@ import com.growmighty.lectures.firstday.payment.config.PaymentRecoveryProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @EnableScheduling
 @EnableConfigurationProperties(PaymentRecoveryProperties.class)
+@EntityScan(basePackages = {
+    "com.growmighty.lectures.firstday.payment.domain",
+    "com.growmighty.lectures.firstday.refund.domain"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.growmighty.lectures.firstday.payment.infrastructure",
+    "com.growmighty.lectures.firstday.refund.infrastructure"
+})
 @SpringBootApplication(scanBasePackages = {
     "com.growmighty.lectures.firstday.payment",
     "com.growmighty.lectures.firstday.refund",
