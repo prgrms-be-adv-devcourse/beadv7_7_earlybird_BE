6312a8bb2cbe2288b1f3dcb3c2bbb2d1f458782f
Kang, DaeHyuk <kangdh0430@naver.com>
2026-08-06 10:14:20 +0900

    Fix: StockChangeLogRepositoryTest에서 불필요한 @DirtiesContext 제거

--- project-service/src/test/java/com/growmighty/lectures/firstday/project/reward/infrastructure/StockChangeLogRepositoryTest.java
+++ project-service/src/test/java/com/growmighty/lectures/firstday/project/reward/infrastructure/StockChangeLogRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -16,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class StockChangeLogRepositoryTest extends MySqlIntegrationTestSupport {
 
     @Autowired
