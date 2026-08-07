f6208f15467e61ea9ec74a2a7fb1d5e89fa06b99
Jung's Study <JungsArk@gmail.com>
2026-08-07 14:59:57 +0900

    feat : 환불 Gateway 오류 분류와 SAGA 실패 처리 추가

--- payment-service/src/main/java/com/growmighty/lectures/firstday/refund/application/RefundCancellationSagaOrchestrator.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/refund/application/RefundCancellationSagaOrchestrator.java
@@ -1,6 +1,8 @@
 package com.growmighty.lectures.firstday.refund.application;
 
 import com.growmighty.lectures.firstday.refund.application.dto.RefundCancellationTarget;
+import com.growmighty.lectures.firstday.refund.application.exception.RefundGatewayException;
+import com.growmighty.lectures.firstday.refund.application.exception.RefundGatewayFailureType;
 import com.growmighty.lectures.firstday.refund.application.port.RefundGateway;
 import com.growmighty.lectures.firstday.refund.domain.RefundReason;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +17,16 @@ public class RefundCancellationSagaOrchestrator {
     // 환불 시작, Toss 취소, 내부 완료 상태 전이를 순서대로 처리
     public void cancel(Long orderId, RefundReason reason) {
         RefundCancellationTarget target = refundService.startRefund(orderId, reason);
+        try {
+            refundGateway.refund(target.paymentKey(), target.reason());
 
-        refundGateway.refund(target.paymentKey(), target.reason());
+            refundService.completeRefund(target.refundId());
+        } catch (RefundGatewayException exception) {
+            if (exception.getFailureType() == RefundGatewayFailureType.DEFINITIVE) {
+                refundService.failRefund(target.refundId());
+            }
 
-        refundService.completeRefund(target.refundId());
+            throw exception;
+        }
     }
 }
