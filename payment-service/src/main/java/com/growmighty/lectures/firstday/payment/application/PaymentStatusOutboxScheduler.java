a52c510d4631852ff1dacf3fee7caf0ce0c09965
Jung's Study <JungsArk@gmail.com>
2026-08-19 12:01:45 +0900

    fix : outbox 발행 스케쥴러 재조정

--- payment-service/src/main/java/com/growmighty/lectures/firstday/payment/application/PaymentStatusOutboxScheduler.java
+++ payment-service/src/main/java/com/growmighty/lectures/firstday/payment/application/PaymentStatusOutboxScheduler.java
@@ -10,7 +10,7 @@ public class PaymentStatusOutboxScheduler {
 
     private final PaymentStatusOutboxBatchService paymentStatusOutboxBatchService;
 
-    @Scheduled(fixedDelayString = "${payment.outbox.schedule-fixed-delay:60000}")
+    @Scheduled(fixedDelayString = "${payment.outbox.schedule-fixed-delay:2000}")
     public void dispatchPendingOutboxes() {
         paymentStatusOutboxBatchService .dispatchPendingOutboxes();
     }
